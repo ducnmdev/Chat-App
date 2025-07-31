51aca3d132a870c78d1dd51aff00e158115efa58
ducnm168 <ductiny2003@gmail.com>
2025-08-01 00:30:08 +0700

    create models, routes, controllers, add logic, create globle state with zustand...

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,23 @@
-import express from "express"
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
+import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
+dotenv.config();
 const app = express();
 
-app.use('/api/auth', authRoutes)
+const PORT = process.env.PORT;
 
-app.listen(5001, () => {
-    console.log('server is running on port 5001')
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/messages', messageRoutes);
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
+    connectDB();
 })
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,17 +1,15 @@
 import express from "express";
+import { login, logout, signup, updateProfile, checkAuth } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js"
 
 const router = express.Router();
 
-router.post("/signup", (req, res) => {
-    res.send("signup route")
-});
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
 
-router.post("/login", (req, res) => {
-    res.send("login route")
-});
+router.put("/update-profile", protectRoute, updateProfile);
 
-router.post("/logout", (req, res) => {
-    res.send("logout route")
-});
+router.get("/check", protectRoute, checkAuth);
 
 export default router;
