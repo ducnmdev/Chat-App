6b3e3661ca6dce9d757eb129522057a00e0aa6cc
ducnm168 <ductiny2003@gmail.com>
2025-08-06 01:34:36 +0700

    final fix

--- frontend/src/store/useThemeStore.js
+++ frontend/src/store/useThemeStore.js
@@ -2,6 +2,7 @@
 
 export const useThemeStore = create((set) => ({
   theme: localStorage.getItem("chat-theme") || "coffee",
+  
   setTheme: (theme) => {
     localStorage.setItem("chat-theme", theme);
     set({ theme });
